ef6de5c92ca2a809467ad8ee76eeb5bf870a80fc
Tasfik Rahman <tasfik.rahman@dsinnovators.com>
2021-08-08 13:49:10 +0600

    liquibase integrated. Closes #1

---
+++ src/main/java/com/tasfik007/backendecommerce/BackendEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.tasfik007.backendecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendEcommerceApplication.class, args);
+    }
+
+}
