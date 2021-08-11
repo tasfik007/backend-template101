a87271d6feebd9a30d95c337c84951af3bf5d2f6
Tasfik Rahman <tasfik.rahman@dsinnovators.com>
2021-08-11 17:34:07 +0600

    POJOs converted into JPA Entities. Closes #4

--- src/main/java/com/tasfik007/backendecommerce/database/models/Customer.java
+++ src/main/java/com/tasfik007/backendecommerce/database/models/Customer.java
@@ -2,12 +2,20 @@ package com.tasfik007.backendecommerce.database.models;
 
 import lombok.*;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
 @EqualsAndHashCode
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String email;
